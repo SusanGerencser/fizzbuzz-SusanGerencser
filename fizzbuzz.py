719c5a1966591749978b5c0a4d41bc3eb1527963
Susan <susan.gerencser@alumni.wilkes.edu>
2024-07-28 13:33:27 -0400

    Add existing project files to Git

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
-
+for num in range(1,101):
+  
+  if(num%3==0 and num%5==0):
+    print("FizzBuzz")
+  
+  if(num%3 == 0):
+    print("Fizz")
+  
+  if(num%5 == 0):
+    print("Buzz")
+  
+  else:
+    print(num)
